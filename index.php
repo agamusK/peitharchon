a8c53a99e3477496c6c71fd28b9a37a23a011a64
agamusK <fakedoors@gmail.com>
2014-12-09 02:05:42 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Name of Your Library</title>
+<script src="js/menu.js" type="text/javascript"></script>
+<link href="shelves.css" rel="stylesheet" type="text/css" />
+<link REL="SHORTCUT ICON" HREF="<!-- Add your website icon address here -->"> 
+<style type="text/css">
+<!--
+.heading {
+	color: #F90;
+	font-family: "calibri";
+}
+.options {
+	font-family: "calibri";
+	font-size: 16px;
+	font-style: oblique;
+	color: #F93;
+}
+-->
+</style>
+</head>
+
+<body>
+
+<br />
+<br />
+<br />
+
+<table align="center" cellpadding="0" bgcolor="#FFFFFF" width="900" border="0">
+  <tr>
+    <td><h1 align="center" class="heading"><img src="/akml/gfx/kmlheader.png" width="830" height="51" alt="AKML Welcome" /></h1>
+      <p align="center"><a href="/akml/search.php"><img border="0" src="/akml/gfx/search.png" onmouseover="this.src='/akml/gfx/search2.png';" onmouseout="this.src='/akml/gfx/search.png';" /></a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="about.php"><img border="0" src="/akml/gfx/about.png" onmouseover="this.src='/akml/gfx/about2.png';" onmouseout="this.src='/akml/gfx/about.png';" /></a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="/akml/admin/"><img border="0" src="/akml/gfx/admin.png" onmouseover="this.src='/akml/gfx/admin2.png';" onmouseout="this.src='/akml/gfx/admin.png';" /></a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="<!-- Add your website address here -->"><img border="0" src="/akml/gfx/online.png" onmouseover="this.src='/akml/gfx/online2.png';" onmouseout="this.src='/akml/gfx/online.png';" /></a></p>
+	  <p align="center">
+   	  <table align="center" cellpadding="0" bgcolor="#FFFFFF" width="890" border="1">
+		<tr>
+		  <td>
+			<div id="sse1">
+			<div id="sses1">
+				<ul>
+					<li><a href="/akml/pages/critical.php">Secondary</a></li>
+					<li><a href="/akml/pages/classics.php">Classics</a></li>
+					<li><a href="/akml/pages/history.php">History</a></li>
+					<li><a href="/akml/pages/literature.php">Literature</a></li>
+					<li><a href="/akml/pages/nonfiction.php">Non-Fiction</a></li>
+					<li><a href="/akml/pages/philosophy.php">Philosophy</a></li>
+					<li><a href="/akml/pages/poetry.php">Poetry</a></li>
+					<li><a href="/akml/pages/science.php">Science</a></li>
+					<li><a href="/akml/pages/theater.php">Theatre</a></li>
+				</ul>
+			</div>
+			</div>
+		  </td>
+		</tr>
+	  </table>        
+      </p>
+  <p align="center">&nbsp;</p>
+        <center>
+        <a href="about.php"><img src="/akml/gfx/kmlfooter.jpg" alt="Hwaet! Productions" width="238" height="25" border="0" /></a>
+      </center>
+    <p align="left">&nbsp;</p></td>
+  </tr>
+</table>
+<h1 align="center" class="heading">&nbsp;</h1>
+<!-- Wonder, for in this is your church.  Here is a notion for to place unto your noggin-space: "In a climate that is very provocative, tea is not advisable for a beginning:  one should begin an hour earlier with a cup of thick, oil-less cocoa."  Nietzsche, F.; Ecce Homo.; "Why I am so Clever".-->
+</body>
+</html>
